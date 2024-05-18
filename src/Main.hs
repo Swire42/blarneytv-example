1b68cd326c9f4d1a5941cc035be9d370b6bf667d
Victor Miquel <v_m_1732@orange.fr>
2024-05-18 21:29:11 +0200

    Implemented the counters system

--- src/Main.hs
+++ src/Main.hs
@@ -1,107 +1,48 @@
-import System.Directory
-import System.Environment
-
 import Blarney
+import Blarney.Nat
+import qualified Blarney.SVec as V
 
-import Control.Arrow ((***))
+type N = 2
+type B1 = Bit 1
+type Vec = V.SVec
 
-import Data.IntMap.Lazy (IntMap)
-import qualified Data.IntMap.Lazy as IntMap
-import Data.IntSet (IntSet)
-import qualified Data.IntSet as IntSet
+incr :: Vec N B1 -> Vec N B1
+incr xs = V.map fst (V.scanl (\(_, en) x -> (en .^. x, en .&. x)) (undefined, 1) xs)
 
-halfAdder :: Bit 1 -> Bit 1 -> (Bit 1, Bit 1)
-halfAdder a b = (a .&. b, a .^. b)
+decr :: Vec N B1 -> Vec N B1
+decr xs = V.map fst (V.scanl (\(_, en) x -> (en .^. x, en .&. inv x)) (undefined, 1) xs)
 
-fullAdder :: Bit 1 -> Bit 1 -> Bit 1 -> (Bit 1, Bit 1)
-fullAdder a b cIn = (cOut, s1)
-  where (c0, s0) = halfAdder a b
-        (c1, s1) = halfAdder s0 cIn
-        cOut = c0 .|. c1
-
-adder2UnrollSeq :: (Bit 1, Bit 1) -> (Bit 1, Bit 1) -> (Bit 1, Bit 1)
-adder2UnrollSeq (a0, a1) (b0, b1) = (out0, out1)
+outer :: B1 -> B1
+outer guard = guard .|. (V.foldr (.&.) 1 $ V.zipWith (.^.) a (decr b))
   where
-    (cOut0, out0) = fullAdder a0 b0 cIn0
-    (cOut1, out1) = fullAdder a1 b1 cIn1
-    cNext0       = reset0 ? (0, cOut0)
-    cNext1       = reset1 ? (0, cOut1)
-    cIn0         = delay 0 cNext1
-    cIn1         = cNext0
-    reset0 = delay 0 reset0
-    reset1 = delay 1 reset1
+    a = delay zero $ incr a
+    b = delay zero $ decr b
 
-adder2Comb :: (Bit 1, Bit 1) -> (Bit 1, Bit 1) -> (Bit 1, Bit 1)
-adder2Comb (a0, a1) (b0, b1) = (out0, out1)
+inner :: B1 -> B1
+inner guard = V.foldr (.&.) 1 (V.map (guard .|.) (V.zipWith (.^.) a (decr b)))
   where
-    (cOut0, out0) = fullAdder a0 b0 0
-    (cOut1, out1) = fullAdder a1 b1 cOut0
+    a = delay zero $ incr a
+    b = delay zero $ decr b
 
-ausEqAus :: (Bit 1, Bit 1) -> (Bit 1, Bit 1) -> Action ()
-ausEqAus x y = assert ((adder2UnrollSeq x y) === (adder2UnrollSeq x y)) "aus === aus"
-
-notId :: Bit 1 -> Bit 1
-notId x = s
-  where
-    s = x .^. c
-    p = delay 0 $ delay 1 p
-    c = delay 0 (p .&. s)
-
-adderSeq x y = res
-  where c_in = delay 0 c_out
-        res@(c_out, _) = fullAdder x y c_in
-doom_adderSeq x y = res
-  where c_in = delay 0 s
-        res@(c_out, s) = fullAdder x y c_in
--- | prop
-prop_addSeq x y = assert (adderSeq x y === adderSeq x y)
-                         "Sequential adder equivalent to itself"
-prop_brokenAddSeq x y = assert (adderSeq x y === doom_adderSeq x y)
-                               "Broken seq adder is equivalent to working one"
+propOuter = \x -> assert (outer x) "outer"
+propInner = \x -> assert (inner x) "inner"
 
 main :: IO ()
 main = do
-  -- path to script output directory
-  cwd <- getCurrentDirectory
-  let smtDir = cwd ++ "/Check/"
-  -- generate smt2 scripts
-  --writeSMTScript cnfWrite (\x -> assert ((notId x) === x) "notId === id") "prop" smtDir
-  --writeSMTScript cnfWrite (\x y -> assert (((delay 0 $ adderSeqUnrollBit @2 x y) === (delay 0 $ x + y))) "Inner") "inner" smtDir
-  --writeSMTScript dfltVerifyConf { verifyConfMode = Induction (fixedDepth 2) True } (\x y -> assert (((test_seq x y) === (test_seq x y))) "test_seq === test_seq") "slow_seqseq_2" smtDir
-  --writeSMTScript dfltVerifyConf { verifyConfMode = Induction (fixedDepth 3) True } (\x y -> assert (((test_seq x y) === (test_seq x y))) "test_seq === test_seq") "slow_seqseq_3" smtDir
-  --writeSMTScript dfltVerifyConf { verifyConfMode = Induction (fixedDepth 4) True } (\x y -> assert (((test_seq x y) === (test_seq x y))) "test_seq === test_seq") "slow_seqseq_4" smtDir
-  --writeSMTScript dfltVerifyConf { verifyConfMode = Induction (fixedDepth 4) True } (\x -> (assert ((notId x) === x) "notId === id") >> (assert ((notId x) === x) "notId === id, bis")) "bis" smtDir
+  -- Safe-neighborhood k-induction, concurent, one-size-fits-all
+  verifyDefault (Info, vconfQuiet) propOuter
+  verifyDefault (Info, vconfQuiet) propInner
 
-  --verifyWith cnfEasy (\x y -> assert (((test_comb x y) === (test_seq x y))) "test_comb === test_seq")
-  --verifyWith cnfEasy (\x y -> assert (((test_seq x y) === (test_seq x y))) "test_comb === test_comb")
-  --verifyWith cnfEasy (\x y -> assert ((((delay 0 *** delay 0) $ test_comb x y) === ((delay 0 *** delay 0) $ test_seq x y))) "delay . test_comb === delay . test_seq")
-  --verifyWith cnfEasy (assert (((delay 0 (test6 2 ())) === (delay 0 (test6 2 ())))) "test6: inner delays")
-  --verifyWith cnfEasy (\x y -> assert (((delay 0 (test4 x y)) === (delay 0 (x + y)))) "test4: inner delays")
-  --verifyWith cnfEasy (\x -> (assert ((notId x) === x) "notId === id"))
-  --verifyWith cnfEasy (\x -> (assert ((notId x) === x) "notId === id") >> (assert ((notId x) === x) "notId === id, bis"))
-  --verifyWith cnfEasy (\x y -> assert (adder @16 x y === x + y) "adder === +")
-  --assertBoundedWhole write_screen 3
-  --verifyFixed VerifConf { restrictedStates = True, write = write_screen } (\x y -> assert (((test_seq x y) === (test_seq x y))) "test_comb === test_seq") "sm"
-  --verifyOfflineFixed (verb, VerifConf { restrictedStates = True, write = write_nothing, giveModel = True }, FixedConf { depth = 4 }) (\x y -> assert (((test_seq x y) === (test_seq x y))) "test_seq === test_seq") "a" "Check"
-  --verifyLiveFixed (verb, vconfDefault, fconfCombinational) (\(x :: Bit 1) (y :: Bit 1) -> assert ((inv (x .|. y)) === ((inv x) .&. (inv y))) "De Morgan")
-  verifyLiveIncremental cnf (assert ((0 .&. dontCare) === inv (1 .|. dontCare :: Bit 1)) "don't care val 1 (Holds)")
-  verifyLiveIncremental cnf (assert (0 === (dontCare :: Bit 1)) "don't care val 2 (Falsifiable)")
-  verifyLiveIncremental cnf (assert (dontCare === (dontCare :: Bit 1)) "don't care val 3 (Falsifiable)")
-  verifyLiveIncremental cnf (\(x :: Bit 1) -> assert (delay 0 x === delay dontCare x) "don't care reg 1 (Falsifiable)")
-  verifyLiveIncremental cnf (\(x :: Bit 1) -> assert (delay dontCare x === delay dontCare x) "don't care reg 2 (Falsifiable)")
-  verifyLiveIncremental cnf (\(x :: Bit 1) -> assert (delay 1 x === (delay 1 0 .|. delay dontCare x)) "don't care reg 3 (Holds)")
-  verifyLiveIncremental cnf (\(x :: Bit 2) -> assert (delay ((0 :: Bit 1) # (dontCare :: Bit 1)) x === delay dontCare x) "don't care reg 4 (Falsifiable)")
-  --verifyLiveFixed (verb, VerifConf { restrictedStates = True, write = write_nothing, giveModel = True }, FixedConf { depth = 4 }) (\x y -> assert (((test_seq x y) === (test_seq x y))) "test_seq === test_seq")
-  --verifyLiveIncremental cnf (\x -> assert (((notId x) === (id x))) "notId === id")
-  verifyLiveIncremental cnf (\x y -> assert (((adder2UnrollSeq x y) === (adder2UnrollSeq x y))) "aus === aus")
-  --verifyLiveFixed (Verbose, vconfDefault, FixedConf { depth = 4 }) ausEqAus
-  --verifyWith (dfltVerifyConf { verifyConfMode = Induction (fixedDepth 4) True, verifyConfUser = dfltUserConf { userConfInteractive = False } }) ausEqAus
-  --verifyWith cnfEasy prop_brokenAddSeq
-  verifyLiveIncremental cnf prop_brokenAddSeq
-  where
-    cnfEasy = dfltVerifyConf { verifyConfMode = Induction (IncreaseFrom 1) True, verifyConfUser = dfltUserConf { userConfInteractive = False } }
-    cnfHard = dfltVerifyConf { verifyConfMode = Induction (IncreaseFrom 1) True, verifyConfUser = dfltUserConf { userConfInteractive = True, userConfIncreasePeriod = 4 } }
-    cnfWrite = dfltVerifyConf { verifyConfMode = Induction (fixedDepth 3) True, verifyConfUser = dfltUserConf { userConfInteractive = False } }
+  -- Safe-neighborhood k-induction, fixed depth, single-threaded
+  verifyLiveQIFixed (Info, vconfDefault, FixedConf { depth=1 }) propOuter
+  verifyLiveQIFixed (Info, vconfDefault, FixedConf { depth=1 }) propInner
 
-    verb = Info
-    cnf = (verb, vconfDefault, iconfDefault)
+  -- Loop-free k-induction, single-threaded, incremental
+  --verifyLiveIncremental (Info, vconfDefault, iconfDefault) propOuter
+  --verifyLiveIncremental (Info, vconfDefault, iconfDefault) propInner
+
+  -- Old verification backend, single-threaded, incremental
+  --verifyWith (dfltVerifyConf { verifyConfMode = Induction (IncreaseFrom 1) True, verifyConfUser = dfltUserConf { userConfInteractive = False } }) propOuter
+  --verifyWith (dfltVerifyConf { verifyConfMode = Induction (IncreaseFrom 1) True, verifyConfUser = dfltUserConf { userConfInteractive = False } }) propInner
+
+  return ()
